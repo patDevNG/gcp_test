a833e5d65d5ea1c11987ca02b8aebebfdb695383
Okuns Patrick <cgrleadership@gmail.com>
2021-04-02 14:36:51 +0100

    [add] scheduler endpoint

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const bodyParser =require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const server = express();
-server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(cors());
 server.use(bodyParser.json());
 
-server.post('/test', async(req,res)=>{
-    const result = req.body;
-    res.status(200).json({
-        message:"Successful Sent",
-        data:result
-    })
-})
+server.post("/test", async (req, res) => {
+  const result = req.body;
+  res.status(200).json({
+    message: "Successful Sent",
+    data: result,
+  });
+});
 
+server.post("/scheduler", async (req, res) => {
+  console.log("Scheduler called");
 
-server.listen(process.env.PORT || 8584, ()=>{
-    console.log(`Server started at port :8584`);
-})
+  res.status(200).json({
+    message: "Successful",
+  });
+});
 
-
+server.listen(process.env.PORT || 8584, () => {
+  console.log(`Server started at port :8584`);
+});
